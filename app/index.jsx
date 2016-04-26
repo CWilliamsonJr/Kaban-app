7749803b44a81e16a0dbd1a287b3ba5303f45e88
leo81202 <nucklesix@gmail.com>
2016-04-26 18:37:08 -0400

    added flux implementation Alt

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,12 @@ import './main.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/app.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 
 const app = document.getElementById('app');
+
+persist(alt,storage, 'app');
 
 ReactDom.render(<App />, app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,71 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const TARGET = process.env.npm_lifecycle_event;
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
-process.env.BABEL_ENV=TARGET;
-
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
-    app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
 };
+
+process.env.BABEL_ENV = TARGET;
 
 const common = {
-    resolve:{
-        extensions:['','.js','.jsx']
-    },
-    entry: {
-        app: PATHS.app
-    },
-    output: {
-        path: PATHS.build,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                include: PATHS.app
-            },
-            {
-                test:/\.jsx?$/,
-                loaders:['babel?cacheDirectory'],
-                include:PATHS.app
-            }
-        ]
-    }
+  entry: {
+    app: PATHS.app
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
 };
 
-if (TARGET === 'start' || !TARGET) {
-    module.exports = merge(common, {
-        devtool:'eval-source-map',
-        devServer: {
-            contentBase: PATHS.build,
-            historyApiFallback: true,
-            bot: true,
-            inline: true,
-            progress: true,
-            stats: 'errors-only',
-            host: process.env.HOST,
-            port: process.env.PORT
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin(),
-            new NpmInstallPlugin({
-                save:true
-            })
-        ]
-    });
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // display only errors to reduce the amount of output
+      stats: 'errors-only',
+
+      // parse host and port from env so this is easy
+      // to customize
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
+  });
 }
 
-if (TARGET === 'build') {
-    module.exports = merge(common, {});
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
 }
